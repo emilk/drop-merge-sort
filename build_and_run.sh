78b7c4aedabf96ad5b0b5bb9e3a173effbf7fa3b
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2017-01-04 00:08:17 +0100

    Move docs to be compatible with github.io

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,9 +5,10 @@ rustup default nightly # Has a faster sort than stable as of January 2017
 # export RUST_BACKTRACE=1
 cargo test
 
+# Publish documentation in docs/ folder to be compatible with github pages:
 cargo doc
-rm -rf rustdoc/
-cp -r target/doc rustdoc
+rm -rf docs/
+cp -r target/doc docs
 
 # The slow benchmarks:
 time cargo test --release -- --nocapture --ignored
