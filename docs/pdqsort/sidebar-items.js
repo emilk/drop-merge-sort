6484a5c2a7487ce6d3c1342a0aaa92dbde0784af
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-09 15:32:47 +0800

    Delete docs directory

--- docs/pdqsort/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["sort","Sorts a slice."],["sort_by","Sorts a slice using `compare` to compare elements."],["sort_by_key","Sorts a slice using `f` to extract a key to compare elements by."]]});
